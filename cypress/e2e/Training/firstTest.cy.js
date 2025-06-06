380790243e23bc32decd80886206ffedcd372665
Gayashani Divyanjalee <gayashani.divyanjalee@xyicon.com>
2025-06-06 13:42:40 +0530

    initial comit

---
+++ cypress/e2e/Training/firstTest.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+it('Google Test', function(){
+    cy.visit('https://qaapp.xyicon.com/')
+
+    cy.get('#APjb',{timeout:5000}).type("Automation Step by Step")
+
+})
+
+
+
+
+
+
+
+
+// describe('dfgdfh', () => {
+//     it('.type() - type into a DOM element', () => {
+//         // https://on.cypress.io/type
+//         console.log("Login Test")
+
+//     });
+i
+// })
