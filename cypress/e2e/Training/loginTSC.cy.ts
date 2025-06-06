f084c2308be888e8cb9c8882bd0a80357458ad5c
Gayashani Divyanjalee <gayashani.divyanjalee@xyicon.com>
2025-06-06 15:02:55 +0530

    initial commit

--- cypress/e2e/Training/loginTSC.cy.ts
+++
@@ -1,9 +0,0 @@
-it('login test', function() {
-    cy.visit('https://qaapp.xyicon.com/')
-
-    cy.get('#userName').type('gayashani.divyanjalee@xyicon.com')
-
-    cy.get('#pwInput').type('Gaya@2493802')
-
-    cy.get('.primary').click()
-})
