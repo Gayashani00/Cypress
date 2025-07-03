86f8e009357fd3c3e8f94d53bc74877397f5d4be
Gayashani Divyanjalee <gayashani.divyanjalee@xyicon.com>
2025-07-03 16:11:06 +0530

    changes commit

--- cypress/e2e/Tests/login.cy.ts
+++ cypress/e2e/Tests/login.cy.ts
@@ -3,7 +3,6 @@ import { LoginPage } from '../Pages/loginPage';
 describe('Login Tests', () => {
   const loginPage = new LoginPage();
   
-  //Login Test script
   it('login', () => {
     loginPage.login('gayashani.divyanjalee@xyicon.com', 'Gaya@2493802');
   });
